6694d5d26c9672408a4e75bbfc08978f96f540e2
imarcosenciso <48628072+imarcosenciso@users.noreply.github.com>
2021-04-11 12:40:58 +0200

    hashmap in application service changed

--- AirBV/src/main/java/es/deusto/bspq21e1/server/AirBVService.java
+++ AirBV/src/main/java/es/deusto/bspq21e1/server/AirBVService.java
@@ -15,37 +15,52 @@ import es.deusto.bspq21e1.server.data.Van;
 public class AirBVService {
     
     // Parameters
-    Map<String, User> users = new HashMap<String, User>();
-    Map<String, Van> vans = new HashMap<String, Van>();
-    Map<String, Reservation> reservations = new HashMap<String, Reservation>();
+    private static Map<String, User> usersHM = new HashMap<>();
+    private static Map<String, Van> vansHM = new HashMap<>();
+    private static Map<String, Reservation> reservationsHM = new HashMap<>();
 
     public AirBVService() {
 
     }
 
     public User registerUser(String dni, String name, String email) {
-        users.put(dni, new User(dni, name, email));
+        usersHM.put(dni, new User(dni, name, email));
         
-        DBManager.getInstance().store(users.get(dni));
+        DBManager.getInstance().store(usersHM.get(dni));
         
-        return users.get(dni);
+        return usersHM.get(dni);
     }
     
     public void registerVan(Van van) {
+    	vansHM.put(van.getLicensePlate(), van);
     	DBManager.getInstance().store(van);
     }
 
     public boolean cancelReservation(String code) {
-        if(reservations.get(code) != null) {
-            DBManager.getInstance().delete(reservations.get(code));
-            reservations.remove(code);
+        if(reservationsHM.get(code) != null) {
+            DBManager.getInstance().delete(reservationsHM.get(code));
+            reservationsHM.remove(code);
             return true;
         }
         return false;
     }
     
     public void registerReservation(Date bookingDate, int duration, Van van, User vanRenter) {
-    	DBManager.getInstance().store(new Reservation(bookingDate, duration, van, vanRenter));
+    	Reservation reservation = new Reservation(bookingDate, duration, van, vanRenter);
+    	DBManager.getInstance().store(reservation);
+    	
+    	/* WARNING AHEAD!!
+    	 CAREFULL WITH THE ATRIBUTE "CODE" OF THE RESERVATION CLASS,
+    	 WE ARE NOT SURE WHEN EXACTLY THE AUTOINCREMENTAL CODE IS GENERATED
+    	 (DEFINED IN THE REVIEW CLASS AS PK), WHICH COULD LEAD TO PROBLEMS
+    	 WHEN ACCESSING THE HASHMAP.
+    	 
+    	 Look out for possible null pointers.
+    	 @author: Iñigo Marcos @ 11/04
+    	 */
+    	if( reservation.getCode() != null ) {
+    		reservationsHM.put(reservation.getCode(), reservation);
+    	}
     }
 
 	public ArrayList<Van> searchVans(String location, String pickUp, String returnDate) {
