74c158114452f58fa0699f2d728f0cde3fe56674
amaiiapiiki <amaia.pikatza@opendeusto.es>
2021-04-17 12:13:47 +0200

    Logger in AirBV and AirBVService #BSPQ21E1-76 in progress add work 25m Development

--- AirBV/src/main/java/es/deusto/bspq21e1/server/AirBVService.java
+++ AirBV/src/main/java/es/deusto/bspq21e1/server/AirBVService.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import es.deusto.bspq21e1.server.dao.DBManager;
 import es.deusto.bspq21e1.server.data.Reservation;
 import es.deusto.bspq21e1.server.data.User;
@@ -21,18 +23,28 @@ public class AirBVService {
     private static Map<String, User> usersHM = new HashMap<>();
     private static Map<String, Van> vansHM = new HashMap<>();
     private static Map<String, Reservation> reservationsHM = new HashMap<>();
-
+    private static Logger logger = Logger.getLogger(AirBVService.class.getName());
+    
     public AirBVService() {
 
     }
 
+    /**
+     * Registers a new user in Database and stores it in the map
+     * @param dni -  new user's dni
+     * @param name - new users'name
+     * @param email - new user's email
+     * @return returns the created new User
+     */
     public User registerUser(String dni, String name, String email) {
-        usersHM.put(dni, new User(dni, name, email));
+    	logger.debug("Creating and storing a new User:" + name);
+    	usersHM.put(dni, new User(dni, name, email));
         DBManager.getInstance().store(usersHM.get(dni));
         return usersHM.get(dni);
     }
     
     public void registerVan(Van van) {
+    	logger.debug("Creating and storing the new van:" + van.getLicensePlate());
     	vansHM.put(van.getLicensePlate(), van);
     	usersHM.get(van.getUser().getDni()).addVan(van);
     	van.setUser(usersHM.get(van.getUser().getDni()));
@@ -40,15 +52,18 @@ public class AirBVService {
     }
 
     public boolean cancelReservation(String code) {
+    	logger.debug("Canceling the reservation with code: " + code);
         if(reservationsHM.get(code) != null) {
             DBManager.getInstance().delete(reservationsHM.get(code));
             reservationsHM.remove(code);
             return true;
         }
+        logger.error("Error canceling a reservation, the reservation can't be found on HashMap.");
         return false;
     }
     
     public void registerReservation(Date bookingDate, int duration, Van van, User vanRenter) {
+    	logger.debug("Creating and storing new Reservation");
     	Reservation reservation = new Reservation(bookingDate, duration, van, vanRenter);
     	DBManager.getInstance().store(reservation);
     	
@@ -68,6 +83,7 @@ public class AirBVService {
     }
 
 	public ArrayList<Van> searchVans(String location) {
+		logger.debug("Searching vans with location: "+ location);
 		ArrayList<Van> vanAL = new ArrayList<Van>(DBManager.getInstance().getVansByLocation(location));
 		for( Van v : vanAL ) { vansHM.put(v.getLicensePlate(), v); }
 		
@@ -77,10 +93,12 @@ public class AirBVService {
 	}
 
 	public ArrayList<Reservation> getUserReservations(String dni) {
+		logger.debug("Getting all reservations of user: " + dni);
 		return new ArrayList<Reservation>(DBManager.getInstance().getReservationsByUser(usersHM.get(dni)));
 	}
 
 	public void deleteUser(String dni) {
+		logger.debug("Deleting user with dni: " + dni);
 		if(usersHM.get(dni) != null) {
             usersHM.remove(dni);
         }
