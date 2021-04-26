80e759648c17a25c72314f3d8aa5a2a1c4f0074d
alex-tascon <alejandro.tascon@opendeusto.es>
2021-04-26 19:01:06 +0200

    RemoveVanWindow finished, just logic of removing pending

--- AirBV/src/main/java/es/deusto/bspq21e1/client/gui/RemoveVanWindow.java
+++ AirBV/src/main/java/es/deusto/bspq21e1/client/gui/RemoveVanWindow.java
@@ -1,11 +1,17 @@
 package es.deusto.bspq21e1.client.gui;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 import es.deusto.bspq21e1.client.controller.Controller;
 import es.deusto.bspq21e1.serialization.UserData;
@@ -20,10 +26,12 @@ public class RemoveVanWindow extends JFrame {
 	
 	private JFrame frmRemoveVan = new JFrame();
 	
+	private JLabel lblText;
 	private JButton btnRemove, btnBack;
-	private JList<String> jlVansList = new JList<String>();
+	private JList<String> jlVansList;
 	private JScrollPane scrollVans;
 	private ArrayList<VanData> vans = new ArrayList<VanData>();
+	private DefaultListModel<String> vansList = new DefaultListModel<String>();
 	
 	public RemoveVanWindow(Controller controller, UserData user, JFrame frmMain) {
 		this.controller = controller;
@@ -35,11 +43,61 @@ public class RemoveVanWindow extends JFrame {
 		initialize();
 	}
 	
+	public RemoveVanWindow() {
+		frmRemoveVan.setTitle("Remove Van");
+		frmRemoveVan.setResizable(false);
+		frmRemoveVan.setVisible(true);
+		initialize();
+	}
+	
 	private void initialize() {
-		frmRemoveVan.setBounds(100, 100, 670, 360);
+		frmRemoveVan.setBounds(50, 50, 470, 500);
 		frmRemoveVan.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmRemoveVan.getContentPane().setLayout(null);
 		
-		// Initialize buttons and panel for Jlist
+		jlVansList = new JList<String>();
+		jlVansList.setModel(vansList);
+		jlVansList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		scrollVans = new JScrollPane();
+		scrollVans.setBounds(25, 80, 405, 320);
+		scrollVans.setViewportView(jlVansList);
+		frmRemoveVan.getContentPane().add(scrollVans);
+		
+		btnBack = new JButton("Back");
+		btnBack.setBounds(25, 420, 100, 25);
+		btnBack.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frmMain.setVisible(true);
+				frmRemoveVan.dispose();
+			}
+		});
+		frmRemoveVan.getContentPane().add(btnBack);
+		btnBack.updateUI();
+		
+		btnRemove = new JButton("Remove");
+		btnRemove.setBounds(330, 420, 100, 25);
+		btnRemove.setBackground(java.awt.Color.RED);
+		btnRemove.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VanData van = vans.get(jlVansList.getSelectedIndex());
+				// HACER AQUÍ EL CÓDIGO PARA ELIMINAR LA VAN
+				
+				// HACER AQUÍ EL CÓDIGO PARA ELIMINAR LA VAN
+				frmMain.setVisible(true);
+				frmRemoveVan.dispose();
+			}
+		});
+		frmRemoveVan.getContentPane().add(btnRemove);
+		btnRemove.updateUI();
+		
+		lblText = new JLabel("Choose the van to remove from this list:");
+		lblText.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblText.setBounds(43, 10, 580, 50);
+		frmRemoveVan.getContentPane().add(lblText);
+		lblText.updateUI();
+		
 	}
 }
