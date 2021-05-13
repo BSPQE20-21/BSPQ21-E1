7dec8773a8285d4fe877add31e5105247ff3cdcd
alex-tascon <alejandro.tascon@opendeusto.es>
2021-05-13 13:04:27 +0200

    RegUserWindow's appearance done #BSPQ21E1-87 add work 20m Development

--- src/main/java/es/deusto/bspq21e1/client/gui/RegisterUserWindow.java
+++ src/main/java/es/deusto/bspq21e1/client/gui/RegisterUserWindow.java
@@ -4,9 +4,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JTextField;
 
@@ -14,7 +17,6 @@ import es.deusto.bspq21e1.client.controller.Controller;
 import es.deusto.bspq21e1.serialization.UserData;
 
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
 
 import org.apache.log4j.Logger;
 
@@ -26,11 +28,9 @@ public class RegisterUserWindow extends JFrame{
 	
 	private JFrame frmRegisterYourself = new JFrame();
 
-	private JTextField txtIdNumber;
-	private JTextField txtName;
-	private JTextField txtEmail;
+	private JTextField txtIdNumber, txtName, txtEmail;
 	private JPasswordField passwordField;
-	
+	private JButton btnBack, btnRegister;
 	
 	public RegisterUserWindow(Controller controller) {
 		this.controller = controller;
@@ -51,58 +51,80 @@ public class RegisterUserWindow extends JFrame{
 	}
 	
 	private void initialize() {
-		frmRegisterYourself.setBounds(100, 100, 450, 350);
+		frmRegisterYourself.setBounds(100, 100, 385, 315);
 		frmRegisterYourself.setLocationRelativeTo(null);
 		frmRegisterYourself.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmRegisterYourself.getContentPane().setLayout(null);
 		
 		JLabel lblRegisterTitle = new JLabel(controller.getResourcebundle().getString("register_as_new_user_msg"));
-		lblRegisterTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRegisterTitle.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblRegisterTitle.setBounds(56, 20, 294, 19);
+		lblRegisterTitle.setBounds(49, 20, 294, 19);
 		frmRegisterYourself.getContentPane().add(lblRegisterTitle);
 		
 		//JLabels
 		JLabel lblId = new JLabel(controller.getResourcebundle().getString("id_number_msg"));
-		lblId.setBounds(56, 63, 130, 19);
+		lblId.setBounds(40, 63, 130, 19);
 		frmRegisterYourself.getContentPane().add(lblId);
 		
 		JLabel lblName = new JLabel(controller.getResourcebundle().getString("name_msg"));
-		lblName.setBounds(56, 103, 130, 14);
+		lblName.setBounds(40, 103, 130, 14);
 		frmRegisterYourself.getContentPane().add(lblName);
 		
 		JLabel lblEmail = new JLabel(controller.getResourcebundle().getString("email_msg"));
-		lblEmail.setBounds(56, 141, 130, 14);
+		lblEmail.setBounds(40, 141, 130, 14);
 		frmRegisterYourself.getContentPane().add(lblEmail);
 		
 		JLabel lblPassword = new JLabel(controller.getResourcebundle().getString("password_msg"));
-		lblPassword.setBounds(56, 178, 130, 14);
+		lblPassword.setBounds(40, 178, 130, 14);
 		frmRegisterYourself.getContentPane().add(lblPassword);
 		
 		
 		//txtFields
 		txtIdNumber = new JTextField();
-		txtIdNumber.setBounds(200, 63, 140, 20);
+		txtIdNumber.setBounds(184, 63, 140, 20);
 		frmRegisterYourself.getContentPane().add(txtIdNumber);
 		txtIdNumber.setColumns(10);
 		
 		txtName = new JTextField();
-		txtName.setBounds(200, 101, 140, 20);
+		txtName.setBounds(184, 101, 140, 20);
 		frmRegisterYourself.getContentPane().add(txtName);
 		txtName.setColumns(10);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(200, 139, 140, 20);
+		txtEmail.setBounds(184, 139, 140, 20);
 		frmRegisterYourself.getContentPane().add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		//passwordFields
 		passwordField = new JPasswordField();
-		passwordField.setBounds(200, 176, 140, 19);
+		passwordField.setBounds(184, 176, 140, 19);
 		frmRegisterYourself.getContentPane().add(passwordField);
 		
+		
+		btnBack = new JButton(controller.getResourcebundle().getString("back_button_msg"));
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				goToInitialWindow();
+			}
+		});
+		btnBack.addMouseListener(new MouseAdapter() {
+        	public void mouseEntered(MouseEvent e) {
+        		btnBack.setBounds(28, 229, 150, 29);
+        		btnBack.setBackground(Color.RED);
+        		btnBack.updateUI();
+		    }
+		    public void mouseExited(MouseEvent e) {
+		    	btnBack.setBounds(40, 229, 125, 25);
+		    	btnBack.setBackground(btnRegister.getBackground());
+		    	btnBack.updateUI();
+		    }
+		});
+		btnBack.setBounds(40, 229, 125, 25);
+		frmRegisterYourself.getContentPane().add(btnBack);
+		
 		//Register Button
-		JButton btnRegister = new JButton(controller.getResourcebundle().getString("register_msg"));
+		btnRegister = new JButton(controller.getResourcebundle().getString("register_msg"));
 		btnRegister.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 				
@@ -118,18 +140,18 @@ public class RegisterUserWindow extends JFrame{
 				
 			}
 		});
-		btnRegister.setBounds(175, 229, 110, 28);
-		frmRegisterYourself.getContentPane().add(btnRegister);
-		
-		JButton btnBack = new JButton(controller.getResourcebundle().getString("back_button_msg"));
-		btnBack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				goToInitialWindow();
-			}
+		btnRegister.addMouseListener(new MouseAdapter() {
+        	public void mouseEntered(MouseEvent e) {
+        		btnRegister.setBounds(183, 229, 150, 29);
+        		btnRegister.updateUI();
+		    }
+		    public void mouseExited(MouseEvent e) {
+		    	btnRegister.setBounds(195, 229, 125, 25);
+		    	btnRegister.updateUI();
+		    }
 		});
-		btnBack.setBounds(25, 258, 89, 28);
-		frmRegisterYourself.getContentPane().add(btnBack);
+		btnRegister.setBounds(195, 229, 125, 25);
+		frmRegisterYourself.getContentPane().add(btnRegister);
 		
 		logger.info("RegisterUserWindow well initialized");
 	}
