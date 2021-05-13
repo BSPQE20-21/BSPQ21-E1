84413d8a553dfbb9e21122f34e4adbd89ba2561a
amaiiapiiki <amaia.pikatza@opendeusto.es>
2021-05-13 09:57:36 +0200

    fix doxygen problems

--- docs/files_dup.js
+++ docs/files_dup.js
@@ -1,5 +1,4 @@
 var files_dup =
 [
-    [ "java", "dir_e033298f2156e5e9d9e90a60ada84467.html", "dir_e033298f2156e5e9d9e90a60ada84467" ],
-    [ "test", "dir_13e138d54eb8818da29c3992edef070a.html", "dir_13e138d54eb8818da29c3992edef070a" ]
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
