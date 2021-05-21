f805a274118373da148333e1911d8d712a3ea551
amaiiapiiki <amaia.pikatza@opendeusto.es>
2021-05-21 17:58:22 +0200

    Revert "Revert "testing if the problem is in controller""
    This reverts commit ce3f7aa1bef8857c39e510f00558a5796021bea6.

---
+++ src/test/java/es/deusto/bspq21e1/client/gui/CancelReservationWindowTest.java
@@ -0,0 +1,139 @@
+package es.deusto.bspq21e1.client.gui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Font;
+import java.awt.HeadlessException;
+import java.text.ParseException;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.ListSelectionModel;
+
+import org.apache.log4j.Logger;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.bspq21e1.client.controller.Controller;
+import es.deusto.bspq21e1.client.gui.MainWindow;
+import es.deusto.bspq21e1.serialization.ReservationData;
+import es.deusto.bspq21e1.serialization.UserData;
+import es.deusto.bspq21e1.serialization.VanData;
+import junit.framework.JUnit4TestAdapter;
+
+public class CancelReservationWindowTest {
+
+	static Logger logger = Logger.getLogger(CancelReservationWindowTest.class.getName());
+	
+	Controller controller;
+	UserData user;
+	JFrame frmMain;
+	
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(CancelReservationWindowTest.class);
+	}
+	
+	@Before
+	public void setUp() throws ParseException {
+		try {
+			logger.info("Before tests code execution begins");
+			
+			controller = new Controller("127.0.0.1", "8080", "");
+			user = new UserData("00001111A", "Carlos", "carlos@gmail.com", "admin");
+			frmMain = new MainWindow(controller, user, false);
+			
+			logger.info("Before tests code execution ends properly");
+		} catch(HeadlessException e) {
+			logger.error("You are in ubuntu, it's not posible to do window's test");
+		}
+	}
+	
+	@Test
+	@PerfTest(invocations = 5, threads = 10)
+	@Required(max = 1200, average = 250)
+	public void constructorTest() {
+		try {
+			CancelReservationWindow w = new CancelReservationWindow(controller, user, frmMain, false);
+			
+			assertEquals(w.getClass(), CancelReservationWindow.class);
+			assertTrue(w.getFrmCancelReservation().getTitle().length() > 0);
+			assertTrue(!(w.getFrmCancelReservation().isResizable()));
+			assertFalse(w.getFrmCancelReservation().isVisible());
+			
+			logger.info("CancelReservationWindow's constructor tested");
+			
+			assertTrue(w.getFrmCancelReservation().getBounds().getWidth() > 700);
+			assertTrue(w.getFrmCancelReservation().getBounds().getHeight() > 300);
+			assertEquals(w.getFrmCancelReservation().getDefaultCloseOperation(), JFrame.DISPOSE_ON_CLOSE);
+			assertNull(w.getFrmCancelReservation().getContentPane().getLayout());
+			assertNotNull(w.getFrmCancelReservation().getIconImage());
+			
+			assertTrue(w.getLblTitle().getText().length() > 0);
+			assertEquals(w.getLblTitle().getFont(), new Font("Tahoma", Font.BOLD, 18));
+			assertTrue(w.getLblTitle().getBounds().getWidth() > 275);
+			assertTrue(w.getLblTitle().getBounds().getHeight() > 24);
+			
+			assertFalse(w.getTableModel().isCellEditable(0, 0));
+			
+			assertTrue(w.getScrollReservations().getBounds().getWidth() > 685);
+			assertTrue(w.getScrollReservations().getBounds().getHeight() > 180);
+			
+			assertTrue(w.getBtnBack().getText().length() > 0);
+			assertTrue(w.getBtnBack().getActionListeners().length > 0);
+			assertTrue(w.getBtnBack().getMouseListeners().length > 0);
+			assertTrue(w.getBtnBack().getBounds().getWidth() > 95);
+			assertTrue(w.getBtnBack().getBounds().getHeight() > 20);
+			
+			assertTrue(w.getBtnCancel().getText().length() > 0);
+			assertTrue(w.getBtnCancel().getActionListeners().length > 0);
+			assertTrue(w.getBtnCancel().getMouseListeners().length > 0);
+			assertTrue(w.getBtnCancel().getBounds().getWidth() > 95);
+			assertTrue(w.getBtnCancel().getBounds().getHeight() > 20);
+			
+			assertTrue(w.getReservations().size() == 0);
+			
+			assertTrue(w.getJtReservationsTable().getBounds().getWidth() > 685);
+			assertFalse(w.getJtReservationsTable().getTableHeader().getResizingAllowed());
+			assertFalse(w.getJtReservationsTable().getTableHeader().getReorderingAllowed());
+			
+			logger.info("Initialize method tested");
+			
+			assertFalse(w.getBtnCancel().isEnabled());
+			assertTrue(w.getTableModel().getRowCount() == 0);
+			
+			logger.info("Update lists methods tested");
+		} catch(HeadlessException e) {
+			logger.error("You are in ubuntu, it's not posible to do window's test");
+		}
+	}
+	
+	@Test
+	@PerfTest(invocations = 5, threads = 10)
+	@Required(max = 1200, average = 250)
+	public void updateListTest() {
+		try {
+			CancelReservationWindow w = new CancelReservationWindow(controller, user, frmMain, false);
+			
+			VanData van = new VanData("1234PML", "Furgo", "Fur", "Bilbao", 5, true, false, true, 65, user.getDni());
+			controller.registerUsers(user.getDni(), user.getName(), user.getEmail(), user.getPassword());
+			controller.registerVan(van);
+			ReservationData r = new ReservationData(new Date(), 8, van.getLicensePlate(), user.getDni());
+			controller.registerReservation(new Date(), 8, van, user);
+			
+			w = new CancelReservationWindow(controller, user, frmMain, false);
+			
+			controller.eraseVan(van.getLicensePlate());
+			controller.eraseUser(user.getDni());
+			controller.cancelReservation(r.getCode());
+		} catch (HeadlessException e) {
+			
+		}
+	}
+
+}
---
+++ src/test/java/es/deusto/bspq21e1/client/gui/MainWindowTest.java
@@ -0,0 +1,117 @@
+package es.deusto.bspq21e1.client.gui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Font;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+import org.apache.log4j.Logger;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.bspq21e1.client.controller.Controller;
+import es.deusto.bspq21e1.serialization.UserData;
+import junit.framework.JUnit4TestAdapter;
+
+public class MainWindowTest {
+
+	static Logger logger = Logger.getLogger(MainWindowTest.class.getName());
+	
+	Controller controller;
+	UserData user;
+	
+	MainWindow w;
+	
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(MainWindowTest.class);
+	}
+	
+	@Before
+	public void setUp() {
+		try {
+			logger.info("Before tests code execution begins");
+			
+			controller = new Controller("127.0.0.1", "8080", "");
+			user = new UserData("00001111A", "Carlos", "carlos@gmail.com", "admin");
+			
+			logger.info("Before tests code execution ends properly");
+		} catch (HeadlessException e) {
+			logger.error("You are in ubuntu, it's not posible to do window's test");
+		}
+	}
+	
+	@Test
+	@PerfTest(invocations = 5, threads = 10)
+	@Required(max = 1200, average = 250)
+	public void constructorTest() {
+		try {
+			w = new MainWindow(controller, user, false);
+			
+			assertEquals(w.getClass(), MainWindow.class);
+			assertFalse(w.getFrmMain().isResizable());
+			assertFalse(w.getFrmMain().isVisible());
+			
+			logger.info("MainWindow's constructor tested");
+			
+			assertTrue(w.getFrmMain().getBounds().getWidth() > 250);
+			assertTrue(w.getFrmMain().getBounds().getHeight() > 400);
+			assertEquals(w.getFrmMain().getDefaultCloseOperation(), JFrame.DISPOSE_ON_CLOSE);
+			assertNull(w.getFrmMain().getContentPane().getLayout());
+			assertNotNull(w.getFrmMain().getIconImage());
+			
+			assertTrue(w.getLblTitle().getText().length() > 0);
+			assertTrue(w.getLblTitle().getBounds().getWidth() > 295);
+			assertTrue(w.getLblTitle().getBounds().getHeight() > 25);
+			assertEquals(w.getLblTitle().getFont(), new Font("Tahoma", Font.BOLD, 18));
+			
+			assertTrue(w.getBtnSearchVan().getText().length() > 0);
+			assertTrue(w.getBtnSearchVan().getBounds().getWidth() > 170);
+			assertTrue(w.getBtnSearchVan().getBounds().getHeight() > 22);
+			assertTrue(w.getBtnSearchVan().getActionListeners().length > 0);
+			assertTrue(w.getBtnSearchVan().getMouseListeners().length > 0);
+			
+			assertTrue(w.getBtnRegisterVan().getText().length() > 0);
+			assertTrue(w.getBtnRegisterVan().getBounds().getWidth() > 170);
+			assertTrue(w.getBtnRegisterVan().getBounds().getHeight() > 22);
+			assertTrue(w.getBtnRegisterVan().getActionListeners().length > 0);
+			assertTrue(w.getBtnRegisterVan().getMouseListeners().length > 0);
+			
+			assertTrue(w.getBtnCancelRes().getText().length() > 0);
+			assertTrue(w.getBtnCancelRes().getBounds().getWidth() > 170);
+			assertTrue(w.getBtnCancelRes().getBounds().getHeight() > 22);
+			assertTrue(w.getBtnCancelRes().getActionListeners().length > 0);
+			assertTrue(w.getBtnCancelRes().getMouseListeners().length > 0);
+			
+			assertTrue(w.getBtnRemoveVan().getText().length() > 0);
+			assertTrue(w.getBtnRemoveVan().getBounds().getWidth() > 170);
+			assertTrue(w.getBtnRemoveVan().getBounds().getHeight() > 22);
+			assertTrue(w.getBtnRemoveVan().getActionListeners().length > 0);
+			assertTrue(w.getBtnRemoveVan().getMouseListeners().length > 0);
+			
+			assertTrue(w.getBtnRemoveAccount().getText().length() > 0);
+			assertTrue(w.getBtnRemoveAccount().getBounds().getWidth() > 170);
+			assertTrue(w.getBtnRemoveAccount().getBounds().getHeight() > 22);
+			assertTrue(w.getBtnRemoveAccount().getActionListeners().length > 0);
+			assertTrue(w.getBtnRemoveAccount().getMouseListeners().length > 0);
+			
+			assertTrue(w.getBtnlogOut().getText().length() > 0);
+			assertTrue(w.getBtnlogOut().getBounds().getWidth() > 170);
+			assertTrue(w.getBtnlogOut().getBounds().getHeight() > 22);
+			assertTrue(w.getBtnlogOut().getActionListeners().length > 0);
+			assertTrue(w.getBtnlogOut().getMouseListeners().length > 0);
+			
+			logger.info("Initialize method tested");
+ 			
+		} catch(HeadlessException e) {
+			logger.error("You are in ubuntu, it's not posible to do window's test");
+		}
+	}
+}
